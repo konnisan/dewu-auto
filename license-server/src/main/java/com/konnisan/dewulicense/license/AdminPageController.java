5bf30d8583e9a814568dad4fb25040a0930ed85e
konni <104560647+konnisan@users.noreply.github.com>
2026-09-10 23:55:11 +0800

    feat: add admin page route

---
+++ license-server/src/main/java/com/konnisan/dewulicense/license/AdminPageController.java
@@ -0,0 +1,12 @@
+package com.konnisan.dewulicense.license;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminPageController {
+    @GetMapping("/admin")
+    public String admin() {
+        return "redirect:/admin/";
+    }
+}
