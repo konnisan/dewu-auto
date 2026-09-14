02ab00d0cb4b32a696ab5cec28b0c37d0e091b27
konni <104560647+konnisan@users.noreply.github.com>
2026-09-14 10:50:19 +0800

    feat: inject license API into Android build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
+val licenseApiBaseUrl = providers.gradleProperty("LICENSE_API_BASE_URL")
+    .orElse(providers.environmentVariable("LICENSE_API_BASE_URL"))
+    .orElse("")
+
 android {
     namespace = "com.konnisan.dewuauto"
     compileSdk = 36
@@ -14,7 +18,11 @@ android {
         versionCode = 1
         versionName = "1.0.0"
 
-        buildConfigField("String", "LICENSE_API_BASE_URL", "\"\"")
+        buildConfigField(
+            "String",
+            "LICENSE_API_BASE_URL",
+            "\"${licenseApiBaseUrl.get()}\"",
+        )
     }
 
     buildTypes {
